943bce5d60e49d101cece9ee0d450255ee332156
ayazvinski <a.yazvinski@limitless.pl>
2023-09-09 12:30:24 +0200

    UserDao

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,22 +1,6 @@
 package pl.coderslab;
 
-import pl.coderslab.entity.DAO.UserDao;
-import pl.coderslab.entity.User;
-
 public class Main {
-
-    public static void main(String[] args) {
-
-        User user1 = new User();
-        user1.setUserName("JanNowak");
-        user1.setEmail("j.nowak@email.test");
-        user1.setPassword("nowak1990");
-
-        UserDao userDao1 = new UserDao();
-        userDao1.create(user1);
-
-
-    }
 
 
 }
